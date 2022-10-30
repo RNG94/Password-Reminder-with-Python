8e4f26f8e2651eabfc7e4727c797f78780a8a7a2
RNG94 <rabiagunaydi@gmail.com>
2022-10-31 01:36:42 +0300

    New file

---
+++ Assignment-9 (Python - Password Reminder).py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+name = "Rabia"
+write_name = input("write your name: ")
+
+while True:
+    if name == write_name:
+        print("Hello, Rabia! The password is :rng456 ")
+    else:
+        print("Hello, {}! See you later.".format(write_name))
+    break
+    
+    
+    
+
+
+# In[ ]:
+
+
+
+
